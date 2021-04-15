3e903069c103656c4d6742214d8777fdfa54a540
Joyce <hellojoycewong@gmail.com>
2021-04-15 13:40:20 -0700

    updated, post, put, delete route for companies

--- 35.1_Node_pg_Intro/routes/companies.js
+++ 35.1_Node_pg_Intro/routes/companies.js
@@ -29,5 +29,52 @@ router.get("/:code", async (req, res, next) => {
     }
 })
 
+router.post("/", async (req, res, next) => {
+    try {
+        let { code, name, description } = req.body;
+        let results = await db.query(`INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description`, [code, name, description]);
+        return res.status(201).json(results.rows[0])        
+    } catch (e) {
+        next(e)
+    }
+
+})
+
+router.put("/:code", async function (req, res, next) {
+    try {
+        let { name, description } = req.body;
+        let code = req.params.code;
+        
+        const result = await db.query(
+          `UPDATE companies
+           SET name=$1, description=$2
+           WHERE code = $3
+           RETURNING code, name, description`,
+        [name, description, code]);
+        
+        if (result.rows.length === 0) {
+            throw new ExpressError("No such company code", 404)
+        } else {
+        return res.json(result.rows[0])            
+        }
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.delete("/:code", async (req, res, next) => {
+    try {
+        const code = req.params.code;
+        let result = db.query('DELETE FROM companies WHERE code = $1', [code])
+
+        if (result.rows.length === 0) {
+            throw new ExpressError("No such company code", 404)
+        } else {
+        return res.send({ status: "DELETED" })          
+        }                
+    } catch (e) {
+        next(e)
+    }
+})
 
 module.exports = router;
