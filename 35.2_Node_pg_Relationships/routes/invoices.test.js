52b8b02bd9d2b46eeaa55c6ed1033463e2bbc13f
Joyce <hellojoycewong@gmail.com>
2021-04-20 12:00:37 -0700

    write get/post test for / invoices

---
+++ 35.2_Node_pg_Relationships/routes/invoices.test.js
@@ -0,0 +1,38 @@
+process.env.NODE_ENV = "test";
+
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db");
+const {createData} = require("../fakeDB")
+
+// add test before each test
+beforeEach(createData);
+
+afterAll(async () => {
+  await db.end()
+})
+
+
+describe("/GET /invoices", function () {
+    test("Respond with all invoices", async () => {
+        const res = await request(app).get("/invoices");
+        expect(res.statusCode).toBe(200);
+        expect(res.body.length).toEqual(3);
+    });
+})
+
+describe("/POST /invoices", function () {
+    test("test posting a new invoice", async () => {
+        const res = await request(app)
+        .post("/invoices")
+        .send({
+            "comp_code": "apple",
+            "amt": 9876,
+            "paid": false,
+            "add_date": "2021-03-20",
+            "paid_date": "2021-04-20"
+        });
+        expect(res.statusCode).toBe(200)
+        expect(res.body.invoice['amt']).toEqual(9876)
+    })
+})
